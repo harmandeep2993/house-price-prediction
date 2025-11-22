a3005e1019d0108c46251151a117ca8c2bc20bec
harmandeep2993 <harmandeep2993@gmail.com>
2025-11-22 23:46:28 +0100

    add load_config utility function and integrate YAML config loading

---
+++ src/utils/config_loader.py
@@ -0,0 +1,14 @@
+import os 
+import yaml
+
+# Define fucntion to load config.yaml
+def load_config(config_path:str):
+    
+    """
+    Load YAMl config file and return a Python dictionary.
+    """
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Config file not found: {config_path}")0
+    
+    with open(config_path, "r") as f:
+        return yaml.safe_load(f)
