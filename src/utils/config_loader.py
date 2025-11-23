06e0babe29ce595f6e871fc3aa1f140d98a1de9a
harmandeep2993 <harmandeep2993@gmail.com>
2025-11-23 17:39:16 +0100

    fix: remove unwanted character from file

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -8,7 +8,7 @@ def load_config(config_path:str):
     Load YAMl config file and return a Python dictionary.
     """
     if not os.path.exists(config_path):
-        raise FileNotFoundError(f"Config file not found: {config_path}")0
+        raise FileNotFoundError(f"Config file not found: {config_path}")
     
     with open(config_path, "r") as f:
         return yaml.safe_load(f)
